0381c77b7f422a5b27ecc5761cc3fd883c849e5f
Bluette1 <marylene.sawyer@gmail.com>
2024-11-01 07:44:03 +0200

    chore: Configure for Deployment
    - Set up for Deployment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,12 @@
 require 'sidekiq'
 require 'sidekiq-scheduler'
 
+redis_url = ENV['REDIS_URI'] || 'redis://localhost:6379/0'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
+  config.redis = { url: redis_url }
 end
