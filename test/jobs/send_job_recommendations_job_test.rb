24c30769b550a6b92216363ac5d8e976c3086514
Bluette1 <marylene.sawyer@gmail.com>
2025-01-18 00:50:27 +0200

    chore: Run Rubocop
    - Run Linter
    
    Issue [#18](https://github.com/Bluette1/postbee-tracker-api/issues/18)

--- test/jobs/send_job_recommendations_job_test.rb
+++ test/jobs/send_job_recommendations_job_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 describe SendJobRecommendationsJob do
   # it "does a thing" do
--- test/mailers/job_recommendation_mailer_test.rb
+++ test/mailers/job_recommendation_mailer_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 describe JobRecommendationMailer do
   # it "does a thing" do
