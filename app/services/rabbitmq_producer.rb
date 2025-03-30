d2c860f046ea618b2d921f9e7172d735475c0942
Bluette1 <marylene.sawyer@gmail.com>
2025-03-30 21:56:34 +0200

    fix: RabbitMQ TLS
    - Fix RabbitMQ TLS

--- app/services/rabbitmq_producer.rb
+++ app/services/rabbitmq_producer.rb
@@ -1,8 +1,38 @@
+# require 'bunny'
+
+# class RabbitmqProducer
+#   def self.publish(queue_name, message)
+#     connection = Bunny.new(ENV['RABBITMQ_URL'])
+#     connection.start
+
+#     channel = connection.create_channel
+#     queue = channel.queue(queue_name)
+
+#     queue.publish(message)
+#     puts "Sent #{message}"
+
+#     connection.close
+#   end
+# end
+
 require 'bunny'
 
 class RabbitmqProducer
   def self.publish(queue_name, message)
-    connection = Bunny.new(ENV['RABBITMQ_URL'])
+    # Updated connection with proper TLS settings
+    connection = Bunny.new(
+      host: ENV['RABBITMQ_HOST'],
+      port: ENV['RABBITMQ_PORT'].to_i,
+      vhost: ENV['RABBITMQ_VHOST'],
+      user: ENV['RABBITMQ_USERNAME'],
+      password: ENV['RABBITMQ_PASSWORD'],
+      ssl: true,
+      verify_peer: true
+      # If client certificates are required, uncomment and set these:
+      # tls_cert: ENV['RABBITMQ_CLIENT_CERT'],
+      # tls_key: ENV['RABBITMQ_CLIENT_KEY'],
+      # tls_ca_certificates: [ENV['RABBITMQ_CA_CERT']]
+    )
     connection.start
 
     channel = connection.create_channel
--- app/workers/rabbitmq_consumer.rb
+++ app/workers/rabbitmq_consumer.rb
@@ -2,7 +2,21 @@ require 'bunny'
 
 class RabbitmqConsumer
   def self.start(queue_name)
-    connection = Bunny.new(ENV['RABBITMQ_URL'])
+    # connection = Bunny.new(ENV['RABBITMQ_URL'])
+    # Updated connection with proper TLS settings
+    connection = Bunny.new(
+      host: ENV['RABBITMQ_HOST'],
+      port: ENV['RABBITMQ_PORT'].to_i,
+      vhost: ENV['RABBITMQ_VHOST'],
+      user: ENV['RABBITMQ_USERNAME'],
+      password: ENV['RABBITMQ_PASSWORD'],
+      ssl: true,
+      verify_peer: true
+      # If client certificates are required, uncomment and set these:
+      # tls_cert: ENV['RABBITMQ_CLIENT_CERT'],
+      # tls_key: ENV['RABBITMQ_CLIENT_KEY'],
+      # tls_ca_certificates: [ENV['RABBITMQ_CA_CERT']]
+    )
     connection.start
 
     channel = connection.create_channel
